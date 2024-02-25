7d647d30ad941a6c45193a56daf18c72f83a7264
shebov3 <shehabtarik425@yahoo.com>
2024-02-25 16:27:25 +0200

    asd

---
+++ main.py
@@ -0,0 +1,8 @@
+import smtplib
+from colors import * # Just to color the terminal
+
+print("Login in")
+Email = input(colors.CGREEN+"Email: ")
+Password = input("Password: ")
+
+toEmail = input("To: ")
